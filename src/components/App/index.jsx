664d9af1bcc17e7a26ef112b67aa55cb2af76330
Patrick Ivers <patrick.john.ivers@gmail.com>
2017-08-08 17:07:50 +1000

    New: Header component
    Add responsive header component that displays the grid heading.

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,6 +1,13 @@
-import 'reset-css/reset.css';
 import React from 'react';
+import Header from 'components/Header';
 
-const App = () => <div>App</div>;
+import 'reset-css/reset.css';
+import './styles.css';
+
+const App = () => (
+  <div className="app">
+    <Header />
+  </div>
+);
 
 export default App;
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './styles.css';
+
+const Header = () => (
+  <div className="header">
+    <h1>Women&apos;s Tops</h1>
+  </div>
+);
+
+export default Header;
