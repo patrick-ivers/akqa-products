6371ced73bbc405a152e699c0bcf36d8e6210348
Patrick Ivers <patrick.john.ivers@gmail.com>
2017-08-09 16:05:59 +1000

    New: ProductGrid and ProductCard components
    Implement ProductGrid and ProductCard components with a responsive
    layout and mock data. Also, change from PostCSS to Sass.

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from 'components/Header';
-
+import ProductGrid from 'components/ProductGrid';
 import 'reset-css/reset.css';
-import './styles.css';
+import './styles.scss';
 
 const App = () => (
   <div className="app">
     <Header />
+    <ProductGrid />
   </div>
 );
 
